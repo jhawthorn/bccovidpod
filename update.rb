9cb0141cf0098b5fb7c81bd2466a521d22ce3afe
John Hawthorn <john@hawthorn.email>
2020-11-17 01:17:28 -0800

    Add s3 support, skip the birdsong

--- update.rb
+++ update.rb
@@ -2,8 +2,9 @@ require "net/http"
 require "uri"
 require "rss"
 require "time"
+require "fileutils"
 
-ITEM_LIMIT = 6
+ITEM_LIMIT = 1
 
 uri = URI.parse("https://www.youtube.com/feeds/videos.xml?user=ProvinceofBC")
 response = Net::HTTP.get_response(uri)
@@ -19,16 +20,77 @@ input_items = input.items.select do |input_item|
     title =~ /COVID-19 BC Update|Premier Horgan Update/
 end.first(ITEM_LIMIT)
 
+# for testing
+class FileStorage
+  def exists?(file)
+    File.exist?("data/#{file}")
+  end
+
+  def size_of(file)
+    File.size("data/#{file}")
+  end
+
+  def store(dest, src)
+    FileUtils.cp(src, "data/#{dest}")
+  end
+
+  def base_url
+    "./data"
+  end
+end
+
+# for realsies
+class S3Storage
+  def initialize(bucket_name)
+    @bucket_name = bucket_name
+
+    require "aws-sdk-s3"
+    @client = Aws::S3::Client.new(region: "us-east-1")
+    @resource = Aws::S3::Resource.new(client: @client)
+    @bucket = @resource.bucket(@bucket_name)
+  end
+
+  def exists?(file)
+    @bucket.object(file).exists?
+  end
+
+  def size_of(file)
+    @bucket.object(file).size
+  end
+
+  def store(dest, src)
+    options = {
+      acl: "public-read",
+      content_type: "audio/mpeg"
+    }
+    @bucket.object(dest).upload_file(src, options)
+  end
+
+  def base_url
+    "https://#{@bucket_name}.s3.us-east-1.amazonaws.com"
+  end
+end
+
+if s3_bucket = ENV["S3_BUCKET"]
+  storage = S3Storage.new(s3_bucket)
+else
+  storage = FileStorage.new
+end
+
 input_items.each do |input_item|
   video_id = input_item.id.content[/\Ayt:video:(.*)\z/, 1]
-  src = "https://www.youtube.com/watch?v=#{video_id}"
-  dest = "data/#{video_id}.mp3"
+  src_url = "https://www.youtube.com/watch?v=#{video_id}"
+  original = "tmp/#{video_id}_original.m4a"
+  converted = "tmp/#{video_id}_converted.mp3"
+  dest = "#{video_id}.mp3"
 
-  next if File.exist?(dest)
+  next if storage.exists?(dest)
 
-  tmp = "tmp/#{video_id}.m4a"
-  system(*%W[youtube-dl --ignore-config -f bestaudio[ext=m4a] -o #{tmp} #{src}]) || exit($?.to_i)
-  system(*%W[ffmpeg -i #{tmp} -codec:a libmp3lame -b:a 128k #{dest}]) || exit($?.to_i)
+  system(*%W[youtube-dl --ignore-config -f bestaudio[ext=m4a] -o #{original} #{src_url}]) || exit($?.to_i)
+
+  system(*%W[ffmpeg -y -i #{original} -codec:a libmp3lame -b:a 128k -af silenceremove=2:0.01:-20dB #{converted}]) || exit($?.to_i)
+
+  storage.store(dest, converted)
 end
 
 output = RSS::Maker.make("2.0") do |output|
@@ -62,9 +124,9 @@ output = RSS::Maker.make("2.0") do |output|
 
       item.itunes_explicit = false
 
-      base_url = "https://github.com/jhawthorn/bccovidpod/raw/main" # FIXME
-      item.enclosure.url = "#{base_url}/data/#{video_id}.mp3"
-      item.enclosure.length = File.size("data/#{video_id}.mp3")
+      base_url = storage.base_url
+      item.enclosure.url = "#{base_url}/#{video_id}.mp3"
+      item.enclosure.length = storage.size_of("#{video_id}.mp3")
       item.enclosure.type = "audio/mpeg"
 
       # item.itunes_duration = # FIXME
@@ -74,5 +136,3 @@ end
 
 File.write("feed.xml", output.to_s)
 puts output
-
-
